1db4851b1c7f0eb7fdb6c073589dc6f964eb8a51
Ivan Akimov <litirik.yt@gmail.com>
2023-05-25 15:25:37 +0300

    fdhdfgd

--- main.js
+++ main.js
@@ -6,11 +6,3 @@ const customValues = {
 
   const htmlInput = template(customValues);
   document.getElementById('htmlSection').innerHTML = htmlInput;
-  {{#if argument}}
-  // Код для включения, если предоставленный аргумент является истинным
-{{/if}}
-{{#if argument}}
-  // Код для включения, если предоставленный аргумент является истинным
-{{else}}
-  // Код для включения, если предоставленный аргумент ложный
-{{/if}}
